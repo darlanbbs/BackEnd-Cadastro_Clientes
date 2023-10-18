05552be9a8af8dd96c8f350efe7e32bf1cd28359
Darlan Bomfim <darlanbs05@gmail.com>
2023-10-18 11:25:58 -0300

    Importação das funções

--- src/routes/ClientsRoutes.js
+++ src/routes/ClientsRoutes.js
@@ -1,7 +1,22 @@
 const express = require("express");
-const { getUsers } = require("../controller/ClientsController");
+const {
+  getUsers,
+  getUsersPhysical,
+  getUsersJuridical,
+  getUserPhysical,
+  getUserJuridical,
+} = require("../controller/ClientsController");
 const route = express();
 
+// todos os clientes
 route.get("/clients", getUsers);
+
+//pessoas fisicas
+route.get("/clients/physical", getUsersPhysical);
+route.get("/clients/physical/:id", getUserPhysical);
+
+// pessoas juridicas
+route.get("/clients/juridical", getUsersJuridical);
+route.get("/clients/juridical/:id", getUserJuridical);
 
 module.exports = route;
